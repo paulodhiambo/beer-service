5d0b5994fabb850a0f7b555c40bf9d456b98e0e3
paulodhiambo <paulodhiambo962@gmail.com>
2020-11-12 14:06:59 +0300

    project init

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+**Beer Service Microservice**
---
+++ src/test/java/com/odhiambopaul/beerservice/BeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odhiambopaul.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
