78c537e01a4d2214f6721343cd90401a98d54320
paulodhiambo <paulodhiambo962@gmail.com>
2020-11-12 14:54:49 +0300

    created BeerController. closes #2

---
+++ src/main/java/com/odhiambopaul/beerservice/web/controllers/BeerController.java
@@ -0,0 +1,33 @@
+package com.odhiambopaul.beerservice.web.controllers;
+
+import com.odhiambopaul.beerservice.web.model.BeerDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+  @GetMapping("/{beerId}")
+  public ResponseEntity<BeerDTO> getBeerById(@PathVariable("beerId") UUID beerId) {
+    return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity<BeerDTO> saveNewBeer(@RequestBody BeerDTO beerDTO) {
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+
+  @PutMapping("/{beerId}")
+  public ResponseEntity<BeerDTO> updateBeer(
+      @PathVariable("beerId") UUID beerId, @RequestBody BeerDTO beerDTO) {
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
+  @DeleteMapping("/{beerId}")
+  public ResponseEntity<BeerDTO> deleteBeerById(@PathVariable("beerId") UUID beerId) {
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+}
