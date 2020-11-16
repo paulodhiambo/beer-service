cecb9209d7b80915c7d618937ecc203d64ce0b3c
paulodhiambo <paulodhiambo962@gmail.com>
2020-11-16 14:01:06 +0300

    Added Bean Validation. closes #7

--- src/main/java/com/odhiambopaul/beerservice/web/model/BeerDTO.java
+++ src/main/java/com/odhiambopaul/beerservice/web/model/BeerDTO.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,14 +18,13 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDTO {
-  private UUID id;
-  private Integer version;
-  private OffsetDateTime createdDate;
-  private OffsetDateTime lastModifiedDate;
-  private String beerName;
-  private BeerStyleEnum beerStyle;
-  private Long upc;
-  private BigDecimal price;
+  @Null private UUID id;
+  @Null private Integer version;
+  @Null private OffsetDateTime createdDate;
+  @Null private OffsetDateTime lastModifiedDate;
+  @NotNull private String beerName;
+  @NotBlank private BeerStyleEnum beerStyle;
+  @Positive @NotNull private Long upc;
+  @Positive @NotNull private BigDecimal price;
   private Integer quantityOrdered;
-
 }
