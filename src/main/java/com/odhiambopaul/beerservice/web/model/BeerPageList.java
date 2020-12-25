fcd371814ce30d260a65a446355c815b6d235868
paulodhiambo <paulodhiambo962@gmail.com>
2020-12-25 19:38:25 +0300

    Added Page Json conversion. Closes #14

--- src/main/java/com/odhiambopaul/beerservice/web/model/BeerPageList.java
+++ src/main/java/com/odhiambopaul/beerservice/web/model/BeerPageList.java
@@ -1,16 +1,36 @@
 package com.odhiambopaul.beerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public class BeerPageList extends PageImpl<BeerDTO> {
-  public BeerPageList(List<BeerDTO> content, Pageable pageable, long total) {
-    super(content, pageable, total);
-  }
 
-  public BeerPageList(List<BeerDTO> content) {
-    super(content);
-  }
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public BeerPageList(@JsonProperty("content") List<BeerDTO> content,
+                        @JsonProperty("number") int number,
+                        @JsonProperty("size") int size,
+                        @JsonProperty("totalElements") Long totalElements,
+                        @JsonProperty("pageable") JsonNode pageable,
+                        @JsonProperty("last") boolean last,
+                        @JsonProperty("totalPages") int totalPages,
+                        @JsonProperty("sort") JsonNode sort,
+                        @JsonProperty("first") boolean first,
+                        @JsonProperty("numberOfElements") int numberOfElements) {
+
+        super(content, PageRequest.of(number, size), totalElements);
+    }
+
+    public BeerPageList(List<BeerDTO> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPageList(List<BeerDTO> content) {
+        super(content);
+    }
 }
